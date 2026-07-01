f6073c4d1f5d96ad925a41914ca0444ed1659cca
Reina <reinasosa2024@gmail.com>
2026-07-01 15:11:24 -0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,31 @@ import React from 'react'
 import { Navbar } from './Navbar/Navbar'
 import headerBackgroundVideo from '../assets/headerBackground.mp4';
 import { Link } from 'react-router-dom';
+import OrangeCatCartoon  from '/src/assets/OrangeCatCartoon.png'
 
 export const Header = () => {
   return (
     <>
-    <header className="w-full h-screen flex flex-col">
+    <header className="w-full h-screen flex flex-col text-white font-semibold">
         <Navbar />
         <video autoPlay muted loop playsInline className="absolute top-0 -z-1 w-full h-full object-cover">
             <source src={headerBackgroundVideo} type="video/mp4" />
             Your browser can't reproduce videos
         </video>
 
-        <div className='hero-information flex relative justify-center items-center'>
-            <div className='metrics font-semibold gap-3 text-white mt-auto mb-10'>
-                <Link to="/sign-up" className="link py-2 px-3 rounded-xl border border-secondary text-prymary bg-secondary/30 transition-all duration-300 ease-in-out hover:text-primary hover:bg-secondary">
+        <div className='hero-information flex flex-col relative justify-center items-center font-semibold gap-3 mt-auto mb-15'>   
+            <div className='mx-30 mb-5'>
+                <h1 className='text-center text-secondary text-5xl/15 font-extrabold'>Become a hero for {<img src={OrangeCatCartoon} className='inline-block w-11 h-12 align-baseline'></img>} in need </h1>
+                <h4 className='text-center text-lg/8 font-bold'>Connect with shelters, volunteer your time, and help cats receive the care and love they deserve</h4>
+            </div>
+
+            <div className='hero-buttons flex font-semibold gap-3'>
+                <Link to="/sign-up" className="link py-2 px-3 rounded-xl bg-secondary-light/30 transition-all duration-300 ease-in-out hover:text-primary hover:bg-secondary-light">
                     Become a Volunteer
                 </Link>
-                <Link to="/sign-up" className="link py-2 px-3 rounded-xl border border-secondary text-white bg-primary/30 transition-all duration-300 ease-in-out hover:bg-primary hover:text-primary-light">Register Your Shelter</Link>
+                <Link to="/sign-up" className="link py-2 px-3 rounded-xl bg-primary/40 transition-all duration-300 ease-in-out hover:bg-primary">
+                    Register Your Shelter
+                </Link>
             </div>
         </div>
     </header>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import OrangeLogo from '/src/assets/PawsToTheRescueLogoOrange.png'
 import WhiteLogo from '/src/assets/PawsToTheRescueLogoWhite.png'
-import hero from '/src/assets/orangeCat.png'
 
 export const Navbar = () => {
   return (
     <>
-      <nav className='flex p-4 text-white items-center font-semibold px-10'>
+      <nav className='flex p-4 items-center px-10'>
         <div className='logo mr-auto'>
           <Link to="/Home" className='link'>
             <img src={WhiteLogo} alt="Paws to the Rescue logo" className="w-45 h-auto min-h-12 min-w-33"/>
@@ -15,15 +14,53 @@ export const Navbar = () => {
         </div>
 
         <div className='links flex justify-center gap-4'>
-          <Link to="/home" className="link py-2 px-3 rounded-xl border border-transparent hover:text-secondary hover:bg-primary/50 transition-all duration-300 ease-in-out hover:-translate-y-0.5">Home</Link>
-          <Link to="/volunteering-opportunities" className="link py-2 px-3 rounded-xl border border-transparent hover:text-secondary hover:bg-primary/50 transition-all duration-300 ease-in-out hover:-translate-y-0.5">Volunteering </Link>
-          <Link to="/shelters" className="link py-2 px-3 rounded-xl border border-transparent hover:text-secondary hover:bg-primary/50 transition-all duration-300 ease-in-out hover:-translate-y-0.5" >Shelters </Link>
-          <Link to="/about-us" className="link py-2 px-3 rounded-xl border border-transparent hover:text-secondary hover:bg-primary/50 transition-all duration-300 ease-in-out hover:-translate-y-0.5">About Us </Link>
+          <Link to="/home" className='relative py-2 px-3 transition-all duration-300 hover:text-secondary
+                                  after:absolute
+                                  after:left-0
+                                  after:bottom-0
+                                  after:h-0.5
+                                  after:w-0
+                                  after:bg-secondary
+                                  after:transition-all
+                                  after:duration-300
+                                  hover:after:w-full'>
+                                    Home
+                                    </Link>
+          <Link to="/volunteering-opportunities" className='relative py-2 px-3 transition-all duration-300 hover:text-secondary
+                                  after:absolute
+                                  after:left-0
+                                  after:bottom-0
+                                  after:h-0.5
+                                  after:w-0
+                                  after:bg-secondary
+                                  after:transition-all
+                                  after:duration-300
+                                  hover:after:w-full'>Volunteering </Link>
+          <Link to="/shelters" className='relative py-2 px-3 transition-all duration-300 hover:text-secondary
+                                  after:absolute
+                                  after:left-0
+                                  after:bottom-0
+                                  after:h-0.5
+                                  after:w-0
+                                  after:bg-secondary
+                                  after:transition-all
+                                  after:duration-300
+                                  hover:after:w-full' >Shelters </Link>
+          <Link to="/about-us" className='relative py-2 px-3 transition-all duration-300 hover:text-secondary
+                                  after:absolute
+                                  after:left-0
+                                  after:bottom-0
+                                  after:h-0.5
+                                  after:w-0
+                                  after:bg-secondary
+                                  after:transition-all
+                                  after:duration-300
+                                  hover:after:w-full'>About Us </Link>
         </div>
 
         <div className='button-for-auth ml-auto flex gap-3'>
-          <Link to="/login" className='link py-2 px-3 rounded-xl bg-white text-primary hover:bg-primary-light transition-all duration-300 ease-in-out hover:-translate-y-0.5'>Log In </Link>
-          <Link to="/sign-up" className='link py-2 px-3 rounded-xl bg-primary text-white hover:bg-primary-dark transition-all duration-300 ease-in-out hover:-translate-y-0.5'>Sign Up</Link>
+          <Link to="/login" className='link py-2 px-3 rounded-xl bg-white text-primary hover:bg-secondary-light transition-all duration-300 ease-in-out hover:-translate-y-0.5'>Log In </Link>
+          <Link to="/sign-up" className='link py-2 px-3 rounded-xl bg-primary hover:bg-primary-dark transition-all duration-300 ease-in-out hover:-translate-y-0.5'>Sign Up</Link>
         </div>
       </nav>
     </>
